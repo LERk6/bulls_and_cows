4197221d37131b2e3a6d472a121e39ce4ec43664
lerk6 <evgeniqloginov@gmail.com>
2022-03-21 22:45:55 +0700

    generating_numbers

--- main.go
+++ main.go
@@ -7,27 +7,55 @@ import (
 )
 
 const (
-	digits_in_number = 4
+	Digits_in_number = 4
+	Sleeps_times     = 50
 )
 
 func main() {
 	var (
-		str string
+		str, winning_number string
 	)
 	input(&str)
-	fmt.Println("ИТОГ - " + str)
+	generating_numbers(&winning_number)
+	fmt.Printf("\nИТОГ:   загаданное число - %v \n\tваше число - %v\n\n", winning_number, str)
+}
 
+func generating_numbers(winning_number *string) {
+	temp := int(time.Now().Unix()) * int(time.Now().Unix())
+	exit := 0
+	t := temp % 10
+	*winning_number += fmt.Sprint(t)
+	temp /= 10
+	for i := 1; i < Digits_in_number; i++ {
+		for exit != 1 {
+			t = temp % 10
+			exit = 1
+			for _, v := range *winning_number {
+				if int(v) == t+48 {
+					exit = 0
+
+					break
+				}
+			}
+			temp /= 10
+		}
+		exit = 0
+		*winning_number += fmt.Sprint(t)
+		if temp == 0 {
+			temp = int(time.Now().Unix()) * int(time.Now().Unix()) / 2
+		}
+	}
 }
 
 func input(str *string) {
 	exit := 0
 	for exit != 1 {
+		fmt.Printf("\033c")
 		exit = 0
-		fmt.Print("Введите ", digits_in_number, "-значное число(без повтора цифр): ")
-		for fmt.Scan(str); len(*str) != digits_in_number; fmt.Scan(str) {
-			fmt.Println("val =", *str, "len =", len(*str))
+		fmt.Print("Введите ", Digits_in_number, "-значное число(без повтора цифр): ")
+		for fmt.Scan(str); len(*str) != Digits_in_number; fmt.Scan(str) {
 			incorrect_input(str)
-			fmt.Print("Повторите попытку: ")
+			fmt.Print("Введите ", Digits_in_number, "-значное число(без повтора цифр): ")
 		}
 		if i := fmt.Sprint(strings.ToUpper(*str)); i == "EXIT" {
 			*str = i
@@ -65,13 +93,20 @@ func input(str *string) {
 }
 
 func incorrect_input(str *string) {
+	// fmt.Printf("\033c")
+	loading()
+	fmt.Println("Некорректный ввод! (" + *str + ")")
+	loading()
+	fmt.Printf("\033c")
+}
+
+func loading() {
 	fmt.Print("[")
 	for i := 0; i < 10; i++ {
-		time.Sleep(100 * time.Millisecond)
+		time.Sleep(Sleeps_times * time.Millisecond)
 		fmt.Print(".")
 	}
-	time.Sleep(100 * time.Millisecond)
+	time.Sleep(Sleeps_times * time.Millisecond)
 	fmt.Println("]")
-	time.Sleep(100 * time.Millisecond)
-	fmt.Println("Некорректный ввод! (" + *str + ")")
+	time.Sleep(Sleeps_times * time.Millisecond)
 }
